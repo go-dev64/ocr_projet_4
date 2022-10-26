d1d0d13e22d979c947ce6a15389c8388961fc400
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2022-10-26 16:16:57 +0200

    creation de la class player

---
+++ modeles/match.py
@@ -0,0 +1,10 @@
+
+class Player:
+
+    def __init__(self, name, first_name, date_of_birth, gender, grading):
+        self.name = name
+        self.first_name = first_name
+        self.date_of_birth = date_of_birth
+        self.gender = gender
+        self.grading = grading
+
